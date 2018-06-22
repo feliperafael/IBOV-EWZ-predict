41fc967b1e9ebe7ed256d402d187dcb1ea5c516f
feliperafael <feliperafael_95@yahoo.com.br>
2018-06-22 20:01:28 -0300

    rescaling data

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 from sklearn import linear_model
 from sklearn import neural_network
 from sklearn.model_selection import train_test_split
@@ -14,9 +15,9 @@ def MLPRegressor(X_train, X_test, y_train, y_test):
 		model.append(neural_network.MLPRegressor(hidden_layer_sizes=(100, ),max_iter=1000))
 		model[i].fit(X_train,y_train)
 		y_pred = model[i].predict(X_test)
-		error.append(np.sqrt(mean_squared_error(y_pred, y_test)))
+		error.append(np.sqrt(r2_score(y_pred, y_test)))
 		
-	print("RMSE: %.2f" % np.mean(error))
+	print("RMSE: %.5f" % np.mean(error))
 	return model[np.argmin(error)]
 
 def LinearRegression(X_train, X_test, y_train, y_test):
@@ -24,7 +25,7 @@ def LinearRegression(X_train, X_test, y_train, y_test):
 	reg = linear_model.LinearRegression()
 	reg.fit(X_train,y_train)
 	y_pred = reg.predict(X_test)
-	print("RMSE: %.2f" % np.sqrt(mean_squared_error(y_pred, y_test)))
+	print("RMSE: %.5f" % np.sqrt(r2_score(y_pred, y_test)))
 
 	return reg
 
@@ -35,8 +36,16 @@ def main():
 	file = file.drop(['timestamp','volume','dividend_amount', 'close', 'adjusted_close','split_coefficient'], axis=1)
 	dataset = file.values
 	dataset = dataset.astype('float32')
+	print("yMAX: ",max(y))
+	y = y/max(y)
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	scaler = scaler.fit(dataset)
+	#print('Min: , Max: ', scaler.data_min_, scaler.data_max_)
+	# normalize the dataset and print the first 5 rows
+	dataset = scaler.transform(dataset)
+	#print(dataset)
 
-	X_train, X_test, y_train, y_test = train_test_split(dataset, y, test_size=0.20)
+	X_train, X_test, y_train, y_test = train_test_split(dataset, y, test_size=0.50, random_state=42)
 
 	MLPRegressor(X_train, X_test, y_train, y_test)
 	LinearRegression(X_train, X_test, y_train, y_test)
